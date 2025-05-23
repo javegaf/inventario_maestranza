bb1c745f0b742f32178b54b4df217e69f4ac824f
Javier Vega <ja.vegaf@duocuc.cl>
2025-05-23 18:24:26 -0400

    modelo usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,13 @@
+# usuarios/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    rol = models.CharField(max_length=30, choices=[
+        ('administrador', 'Administrador'),
+        ('gestor', 'Gestor de Inventario'),
+        ('auditor', 'Auditor'),
+        ('logistica', 'Encargado de Logística'),
+        ('comprador', 'Comprador'),
+        ('produccion', 'Jefe de Producción')
+    ])
