beb38f84be78b4c404931593e9a89b89f576cf3a
Javier Vega <ja.vegaf@duocuc.cl>
2025-05-23 18:43:08 -0400

    Agregado el auth secundario para evitar conflictos

--- inventario_maestranza/settings.py
+++ inventario_maestranza/settings.py
@@ -117,3 +117,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'usuarios.Usuario'
